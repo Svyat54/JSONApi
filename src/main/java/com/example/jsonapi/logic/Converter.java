7333a30725f7eef2aa09d830958d2dce94ab9d48
Svyat54 <svytkireev@icloud.com>
2023-01-15 18:07:53 +0700

    + Unit Test

--- src/main/java/com/example/jsonapi/logic/Converter.java
+++ src/main/java/com/example/jsonapi/logic/Converter.java
@@ -10,19 +10,22 @@ public class Converter implements IConverter{
     public JSONMessage.OutputMessage conv(JSONMessage.InputMessage input) {
         String choice = input.choice;
         Integer toDecimal = input.toDecimal;
-        String  inquiry = input.inquiry;
-
-        if(choice.equals("2k10")){
-            return new JSONMessage.OutputMessage(BinaryDecimal.toDecimal(inquiry));
-        }else if(choice.equals("2k16")){
-            return new JSONMessage.OutputMessage(choice ,BinaryHex.toHex(inquiry));
-        }else if(choice.equals("10k2")){
-            return new JSONMessage.OutputMessage(choice ,BinaryDecimal.toBinary(toDecimal));
-        }else if(choice.equals("16k2")){
-            return new JSONMessage.OutputMessage(choice ,BinaryHex.toBinary(inquiry));
-        }else{
+        String inquiry = input.inquiry;
+        try {
+            switch (choice) {
+                case "2k10":
+                    return new JSONMessage.OutputMessage(BinaryDecimal.toDecimal(inquiry));
+                case "2k16":
+                    return new JSONMessage.OutputMessage(choice, BinaryHex.toHex(inquiry));
+                case "10k2":
+                    return new JSONMessage.OutputMessage(choice, BinaryDecimal.toBinary(toDecimal));
+                case "16k2":
+                    return new JSONMessage.OutputMessage(choice, BinaryHex.toBinary(inquiry));
+            }
+        } catch (NotEqualsChoice e) {
             throw new NotEqualsChoice("Not the right choice! Correct 2k10, 2k16, 10k2, 16k2. " +
                     "Choose one and try again");
         }
+        return null;
     }
 }
--- src/test/java/com/example/jsonapi/logic/ConverterTest.java
+++ src/test/java/com/example/jsonapi/logic/ConverterTest.java
@@ -10,14 +10,15 @@ class ConverterTest {
     @Test
     void conv() {
         Converter converter = new Converter();
+        //Совпадение
         assertEquals(converter.conv(new JSONMessage.InputMessage("16k2", null, "10f")).toBinary, "000100001111");
         assertEquals(converter.conv(new JSONMessage.InputMessage("10k2", 168224, null)).toBinary, "101001000100100000");
         assertEquals(converter.conv(new JSONMessage.InputMessage("2k10", null, "1111111")).toDecimal, 127);
         assertEquals(converter.conv(new JSONMessage.InputMessage("2k16", null, "1111111")).hexNumber, "7f");
-
+        //Проверка пустоты
+        assertEquals(converter.conv(new JSONMessage.InputMessage("2k16", null, null)).hexNumber, "is Empty");
+        assertEquals(converter.conv(new JSONMessage.InputMessage("16k2", null, null)).toBinary, "is Empty");
+        //Не совпадение
         assertNotEquals(converter.conv(new JSONMessage.InputMessage("2k16", null, "11111111")).hexNumber, "7f");
-
-
-
     }
 }
